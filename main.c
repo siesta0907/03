680c9f26faea7d45fe6659de3bc5ae4208e2fc3c
siesta0907 <50662414+siesta0907@users.noreply.github.com>
2022-09-20 16:52:45 +0900

    챕터3 실습2

--- main.c
+++ main.c
@@ -1,22 +1,17 @@
 // 2022/09/20
 // IT공학전공 1916513 박희수 
 // main.c
-// chapter 03 실습1 
+// chapter 03 실습 2
 
 #include <stdio.h>
 
 int main(void){
 	
-	int input_int;
-	float input_float;
-	
-	printf("정수를 입력하세요: ");
-	scanf("%d", &input_int);
-	
-	printf("실수를 입력하세요: ");
-	scanf("%f", &input_float);
-	
-	printf("정수: %d, 실수: %f\n ", input_int, input_float);
-	
+	char c;
+	int i;
+	printf("input a number :");
+	scanf("%c", &c);
+	i = c - '0';
+	printf("The input number is %i\n", i);
 	return 0;
 }
